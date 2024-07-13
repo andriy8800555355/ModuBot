9be1499657cf4728d8da6a9324ffb3122c5b2608
andriy8800555355 <83963603+andriy8800555355@users.noreply.github.com>
2024-07-13 16:29:21 +0300

    1

--- StartBotOnLinux.sh
+++ StartBotOnLinux.sh
@@ -6,6 +6,8 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Activate the virtual environment
 source "$script_dir/Linux/bin/activate"
 
+pip install -r https://raw.githubusercontent.com/andriy8800555355/ModuBot/main/NeededLibs.txt
+
 # Change directory to where your app.py is located
 cd "$script_dir"
 
